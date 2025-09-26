e55a876b05a1582e3311b0e66170ff53a8cd2b0e
Francisco <145817626+FranciscoAnticona@users.noreply.github.com>
2025-09-26 18:37:31 -0500

    Muerte

--- Assets/Scripts/Francisco/ControlDeEscena/FadeController.cs
+++ Assets/Scripts/Francisco/ControlDeEscena/FadeController.cs
@@ -1,33 +1,55 @@
 using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI; 
 
 public class FadeController : MonoBehaviour
 {
     public static FadeController Instance;
+    [Header("References")]
     public CanvasGroup fade;
+    public Image fadeImage; 
+
+    [Header("Configuration")]
     public float fadeDuration = 1.0f;
+    public Color defaultFadeColor = Color.black; 
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject); 
         }
         else
         {
             Destroy(gameObject);
         }
+
+        if (fadeImage == null && fade != null)
+        {
+            fadeImage = fade.GetComponent<Image>();
+        }
     }
 
     private void Start()
     {
+        if (fadeImage != null)
+        {
+            fadeImage.color = defaultFadeColor;
+        }
         StartCoroutine(FadeIn());
     }
 
-    public IEnumerator FadeOut(Action onStart = null, Action<float> onUpdate = null, Action onComplete = null)
+    public IEnumerator FadeOut(Action onStart = null, Action<float> onUpdate = null, Action onComplete = null, Color? fadeColor = null)
     {
         onStart?.Invoke();
+
+        Color targetColor = fadeColor ?? defaultFadeColor;
+        if (fadeImage != null)
+        {
+            fadeImage.color = targetColor; 
+        }
 
         float timer = 0f;
         while (timer < fadeDuration)
@@ -46,6 +68,11 @@ public class FadeController : MonoBehaviour
     public IEnumerator FadeIn(Action onStart = null, Action<float> onUpdate = null, Action onComplete = null)
     {
         onStart?.Invoke();
+
+        if (fadeImage != null)
+        {
+            fadeImage.color = defaultFadeColor; 
+        }
 
         float timer = 0f;
         while (timer < fadeDuration)
