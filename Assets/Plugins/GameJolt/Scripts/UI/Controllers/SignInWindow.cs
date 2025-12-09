51225f45b1c2d6cd62e9a8f11b3dcde61eb27ee7
Francisco <145817626+FranciscoAnticona@users.noreply.github.com>
2025-12-09 14:05:36 -0500

    Implementing first trophy

--- Assets/Plugins/GameJolt/Scripts/UI/Controllers/SignInWindow.cs
+++ Assets/Plugins/GameJolt/Scripts/UI/Controllers/SignInWindow.cs
@@ -1,81 +1,110 @@
-﻿using UnityEngine.UI;
+﻿using GameJolt.API;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace GameJolt.UI.Controllers {
-	public class SignInWindow : BaseWindow {
-		public InputField UsernameField;
-		public InputField TokenField;
-		public Text ErrorMessage;
-		public Toggle RememberMeToggle;
-		public Toggle ShowTokenToggle;
+namespace GameJolt.UI.Controllers
+{
+    public class SignInWindow : BaseWindow
+    {
+        public InputField UsernameField;
+        public InputField TokenField;
+        public Text ErrorMessage;
+        public Toggle RememberMeToggle;
+        public Toggle ShowTokenToggle;
 
-		private Action<bool> signedInCallback;
-		private Action<bool> userFetchedCallback;
+        private Action<bool> signedInCallback;
+        private Action<bool> userFetchedCallback;
 
-		public override void Show(Action<bool> callback) {
-			Show(callback, null);
-		}
+        private const int TROPHY_ID_LOGIN = 285303;
 
-		public void Show(Action<bool> signedInCallback, Action<bool> userFetchedCallback) {
-			ErrorMessage.enabled = false;
-			Animator.SetTrigger("SignIn");
-			this.signedInCallback = signedInCallback;
-			this.userFetchedCallback = userFetchedCallback;
-			string username, token;
-			RememberMeToggle.isOn = API.GameJoltAPI.Instance.GetStoredUserCredentials(out username, out token);
-			UsernameField.text = username;
-			TokenField.text = token;
-			ShowTokenToggle.isOn = false;
-		}
+        public override void Show(Action<bool> callback)
+        {
+            Show(callback, null);
+        }
 
-		public override void Dismiss(bool success) {
-			Animator.SetTrigger("Dismiss");
-			if(signedInCallback != null) {
-				signedInCallback(success);
-				signedInCallback = null;
-			}
-		}
+        public void Show(Action<bool> signedInCallback, Action<bool> userFetchedCallback)
+        {
+            ErrorMessage.enabled = false;
+            Animator.SetTrigger("SignIn");
+            this.signedInCallback = signedInCallback;
+            this.userFetchedCallback = userFetchedCallback;
+            string username, token;
+            RememberMeToggle.isOn = API.GameJoltAPI.Instance.GetStoredUserCredentials(out username, out token);
+            UsernameField.text = username;
+            TokenField.text = token;
+            ShowTokenToggle.isOn = false;
+        }
 
-		public void Submit() {
-			ErrorMessage.enabled = false;
+        public override void Dismiss(bool success)
+        {
+            Animator.SetTrigger("Dismiss");
+            if (signedInCallback != null)
+            {
+                signedInCallback(success);
+                signedInCallback = null;
+            }
+        }
 
-			if(UsernameField.text.Trim() == string.Empty || TokenField.text.Trim() == string.Empty) {
-				ErrorMessage.text = "Empty username and/or token.";
-				ErrorMessage.enabled = true;
-			} else {
-				Animator.SetTrigger("Lock");
-				Animator.SetTrigger("ShowLoadingIndicator");
+        public void Submit()
+        {
+            ErrorMessage.enabled = false;
 
-				var user = new API.Objects.User(UsernameField.text.Trim(), TokenField.text.Trim());
-				user.SignIn(signInSuccess => {
-					if(signInSuccess) {
-						Dismiss(true);
-					} else {
-						// Technically this could be because of another user being already signed in.
-						ErrorMessage.text = "Wrong username and/or token.";
-						ErrorMessage.enabled = true;
-					}
+            if (UsernameField.text.Trim() == string.Empty || TokenField.text.Trim() == string.Empty)
+            {
+                ErrorMessage.text = "Empty username and/or token.";
+                ErrorMessage.enabled = true;
+            }
+            else
+            {
+                Animator.SetTrigger("Lock");
+                Animator.SetTrigger("ShowLoadingIndicator");
 
-					Animator.SetTrigger("HideLoadingIndicator");
-					Animator.SetTrigger("Unlock");
-				}, userFetchedSuccess => {
-					if(userFetchedCallback != null) {
-						// This will potentially be called after a user dismissed the window..
-						userFetchedCallback(userFetchedSuccess);
-						userFetchedCallback = null;
-					}
-				}, RememberMeToggle.isOn);
-			}
-		}
+                var user = new API.Objects.User(UsernameField.text.Trim(), TokenField.text.Trim());
+                user.SignIn(signInSuccess => {
+                    if (signInSuccess)
+                    {
 
-		public void ShowToken(bool show) {
-			TokenField.contentType = show ? InputField.ContentType.Standard : InputField.ContentType.Password;
-			TokenField.ActivateInputField();
-		}
+                        Trophies.Unlock(TROPHY_ID_LOGIN, trophySuccess => {
+                            if (trophySuccess)
+                            {
+                                Debug.Log($"[GAMEJOLT] Trophy {TROPHY_ID_LOGIN} unlocked upon successful sign-in.");
+                                Dismiss(true);
+                            }
+                            else
+                            {
+                                Debug.LogError($"[GAMEJOLT] Failed to unlock trophy {TROPHY_ID_LOGIN} upon sign-in.");
+                            }
+                        });
+                    }
+                    else
+                    {
+                        ErrorMessage.text = "Wrong username and/or token.";
+                        ErrorMessage.enabled = true;
+                    }
 
-		public void CreateAccount() {
-			Application.OpenURL("https://gamejolt.com/join");
-		}
-	}
+                    Animator.SetTrigger("HideLoadingIndicator");
+                    Animator.SetTrigger("Unlock");
+                }, userFetchedSuccess => {
+                    if (userFetchedCallback != null)
+                    {
+                        // This will potentially be called after a user dismissed the window..
+                        userFetchedCallback(userFetchedSuccess);
+                        userFetchedCallback = null;
+                    }
+                }, RememberMeToggle.isOn);
+            }
+        }
+
+        public void ShowToken(bool show)
+        {
+            TokenField.contentType = show ? InputField.ContentType.Standard : InputField.ContentType.Password;
+            TokenField.ActivateInputField();
+        }
+
+        public void CreateAccount()
+        {
+            Application.OpenURL("https://gamejolt.com/join");
+        }
+    }
 }
---
+++ Assets/Scripts/Francisco/Gamejolt/MainScene.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using GameJolt.UI;
+using UnityEngine.SceneManagement;
+public class MainScene : MonoBehaviour
+{
+    private void Start()
+    {
+        GameJoltUI.Instance.ShowSignIn((success) =>
+        {
+            if (success)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+            else
+            {
+                Debug.Log("Error");
+            }
+        });
+    }
+}
