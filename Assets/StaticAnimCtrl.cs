e8f1aa8b4044f7b7d967ad1884a20c87f7ee36f1
AndreiMore <andreiloayza14@gmail.com>
2026-04-20 16:27:56 -0500

    Animaciones de Static

---
+++ Assets/StaticAnimCtrl.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class StaticAnimCtrl : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] Material[] originalMaterials;
+    [SerializeField] Material tpMaterial;
+    [SerializeField] SkinnedMeshRenderer mesh;
+
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+
+        if(mesh != null)
+        {
+            originalMaterials = mesh.sharedMaterials;
+        }
+        else
+        {
+            Debug.LogError("No hay SkinnedMeshRenderer asignado en: " + gameObject.name + "!!!");
+        }
+    }
+
+    void Update()
+    {
+        TESTinputs();
+    }
+
+    public void PlayShoot()
+    {
+        anim.SetTrigger("Shoot");
+    }
+
+    public void PlayDeath()
+    {
+        anim.SetTrigger("Death");
+    }
+
+    public void PlayTPout()
+    {
+        Material[] tpArray = new Material[originalMaterials.Length];
+        for(int i = 0; i <tpArray.Length; i++)
+        {
+            tpArray[i] = tpMaterial;
+        }
+
+        mesh.materials = tpArray;
+
+        anim.Play("TP out");
+    }
+
+    public void restoreOriginalMaterials()
+    {
+        mesh.sharedMaterials = originalMaterials;
+    }
+
+    public void PlayTPin()
+    {
+        anim.Play("TP in");
+    }
+
+    void TESTinputs()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) PlayShoot();
+        if(Input.GetKeyDown(KeyCode.Space)) PlayTPout();
+        if(Input.GetKeyDown(KeyCode.M)) PlayTPin();
+        if(Input.GetKeyDown(KeyCode.B)) PlayDeath();
+     }
+
+
+}
