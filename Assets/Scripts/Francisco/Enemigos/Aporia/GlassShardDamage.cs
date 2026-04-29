e5d9f891117bd7c66651e7e028833d192d35b0fa
Francisco <145817626+FranciscoAnticona@users.noreply.github.com>
2026-04-29 00:00:34 -0500

    empuje aporia efecto muerte

--- Assets/Scripts/Francisco/Enemigos/Aporia/GlassShardDamage.cs
+++ Assets/Scripts/Francisco/Enemigos/Aporia/GlassShardDamage.cs
@@ -5,13 +5,22 @@ public class GlassShardDamage : MonoBehaviour
     [HideInInspector] public float damagePerSecond = 4f;
     [HideInInspector] public LayerMask playerLayer;
     [HideInInspector] public float shardDeathDuration = 4f;
+    [SerializeField] private bool effectDeath = false; 
 
     private void OnTriggerStay(Collider other)
     {
         if (((1 << other.gameObject.layer) & playerLayer) == 0) return;
 
         if (other.TryGetComponent<PlayerHealth>(out var pHealth))
+        {
             pHealth.TakeDamage(damagePerSecond * Time.deltaTime);
+            if (effectDeath)       
+            {
+                Vector3 dir = (other.transform.position - transform.position).normalized;
+                dir.y = 0;
+                pHealth.ApplyKnockback(dir, 4, 1f);
+            }
+        }
     }
 
     private void Start()
