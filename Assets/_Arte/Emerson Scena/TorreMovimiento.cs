81e3f2b142f044ed137f49beffc2b92401929832
Emerson Alarcon <emersonalarcon112@gmail.com>
2026-04-29 02:32:39 -0500

    Mejoras

--- Assets/_Arte/Emerson Scena/TorreMovimiento.cs
+++ Assets/_Arte/Emerson Scena/TorreMovimiento.cs
@@ -2,54 +2,38 @@
 
 public class TorreMovimiento : MonoBehaviour
 {
-    public float tiempoDeVida = 2f;
+    public float altura = 2f;
+    public float duracion = 0.3f;
 
     private Vector3 posicionInicial;
-    private Material mat;
-    private float tiempoSubida = 0.3f;
-    private float tiempoActual = 0f;
+    private Vector3 maxHeight;
+    private float tiempo = 0f;
 
     void Start()
     {
         posicionInicial = transform.position;
 
-        // Rotación aleatoria
-        float rotZ = Random.Range(-45f, 45f);
-        transform.rotation = Quaternion.Euler(0, 0, rotZ);
-
-        mat = GetComponent<Renderer>().material;
-
-        // Empieza en dorado (Tint = 1)
-        mat.SetFloat("_Tint", 1f);
+        // Posición objetivo (sube en Y)
+        maxHeight = new Vector3(
+            transform.position.x,
+            transform.position.y + altura,
+            transform.position.z
+        );
     }
 
     void Update()
     {
-        // Subida en Y + cambio de tint
-        if (tiempoActual < tiempoSubida)
+        if (tiempo < duracion)
         {
-            float t = tiempoActual / tiempoSubida;
+            tiempo += Time.deltaTime;
+
+            float t = tiempo / duracion;
 
             transform.position = Vector3.Lerp(
                 posicionInicial,
-                posicionInicial + Vector3.up,
+                maxHeight,
                 t
             );
-
-            // Tint de 1 → 0
-            float tintValue = Mathf.Lerp(1f, 0f, t);
-            mat.SetFloat("_Tint", tintValue);
-
-            tiempoActual += Time.deltaTime;
-        }
-
-        // Tiempo de vida
-        tiempoDeVida -= Time.deltaTime;
-
-        if (tiempoDeVida <= 0)
-        {
-            transform.position = posicionInicial;
-            Destroy(gameObject);
         }
     }
 }
