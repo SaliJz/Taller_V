5a41f8cca1c707543695681bba187b3a25eebe4d
Emerson Alarcon <emersonalarcon112@gmail.com>
2026-04-30 23:59:16 -0500

    mejoras  torre

--- Assets/_Arte/Emerson Scena/TorreMovimiento.cs
+++ Assets/_Arte/Emerson Scena/TorreMovimiento.cs
@@ -1,39 +1,54 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class TorreMovimiento : MonoBehaviour
 {
     public float altura = 2f;
     public float duracion = 0.3f;
+    public float tiempoDeVida = 2f;
 
     private Vector3 posicionInicial;
-    private Vector3 maxHeight;
-    private float tiempo = 0f;
+    private Vector3 posicionFinal;
 
     void Start()
     {
         posicionInicial = transform.position;
 
-        // Posición objetivo (sube en Y)
-        maxHeight = new Vector3(
+        posicionFinal = new Vector3(
             transform.position.x,
             transform.position.y + altura,
             transform.position.z
         );
+
+        StartCoroutine(MovimientoCompleto());
     }
 
-    void Update()
+    IEnumerator MovimientoCompleto()
     {
-        if (tiempo < duracion)
+        float tiempo = 0f;
+
+        while (tiempo < duracion)
         {
             tiempo += Time.deltaTime;
-
             float t = tiempo / duracion;
 
-            transform.position = Vector3.Lerp(
-                posicionInicial,
-                maxHeight,
-                t
-            );
+            transform.position = Vector3.Lerp(posicionInicial, posicionFinal, t);
+            yield return null;
         }
+
+        yield return new WaitForSeconds(tiempoDeVida);
+
+        tiempo = 0f;
+
+        while (tiempo < duracion)
+        {
+            tiempo += Time.deltaTime;
+            float t = tiempo / duracion;
+
+            transform.position = Vector3.Lerp(posicionFinal, posicionInicial, t);
+            yield return null;
+        }
+
+        Destroy(transform.parent.gameObject);
     }
 }
