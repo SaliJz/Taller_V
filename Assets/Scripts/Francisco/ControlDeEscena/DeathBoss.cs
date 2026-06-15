18353888dda847996925935f5bce233ca5a7b19e
Francisco <francisco.anticona81@gmail.com>
2026-06-15 15:16:12 -0500

    Jefe 3, escena final

---
+++ Assets/Scripts/Francisco/ControlDeEscena/DeathBoss.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DeathBoss : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject _BossDeath;
+    [SerializeField] private GameObject _Boss;
+
+    [Header("Settings")]
+    [SerializeField] private float _offsetX = 1;
+    [SerializeField] private float _offsetY = 2;
+    [SerializeField] private float _offsetZ = 0;
+
+    private void OnDestroy()
+    {
+        _BossDeath.transform.position = new Vector3(
+            _Boss.transform.position.x + _offsetX,
+            _Boss.transform.position.y + _offsetY,
+            _Boss.transform.position.z + _offsetZ);
+        _BossDeath.SetActive(true);
+    }
+}
