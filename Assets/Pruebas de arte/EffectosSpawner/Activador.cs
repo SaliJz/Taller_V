9ecfcbefe6bf87d9ccde167819a37bde33cdb820
Rodrigorc2803 <rodrigorc2803@gmail.com>
2026-06-24 11:51:05 -0500

    effectos de golpes boss 1

--- Assets/Pruebas de arte/EffectosSpawner/Activador.cs
+++ Assets/Pruebas de arte/EffectosSpawner/Activador.cs
@@ -1,6 +1,6 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 
 public class Activador : MonoBehaviour
 {
@@ -13,12 +13,17 @@ public class Activador : MonoBehaviour
     [Header("Activación")]
     public bool Activar;
 
+    [Header("Activación en orden")]
+    [SerializeField] private bool EnOrden;
+    [SerializeField] private float tiempoDemora = 0.25f;
+
     private bool yaActivado;
     private Destroy dt;
 
     private void Start()
     {
         dt = GetComponent<Destroy>();
+
         if (setFalseEnStart)
             SetObjetos(false);
     }
@@ -28,9 +33,33 @@ public class Activador : MonoBehaviour
         if (Activar && !yaActivado)
         {
             yaActivado = true;
-            SetObjetos(true);
-            dt.Activar = true;
+
+            if (EnOrden)
+            {
+                StartCoroutine(ActivarEnOrden());
+            }
+            else
+            {
+                SetObjetos(true);
+
+                if (dt != null)
+                    dt.Activar = true;
+            }
         }
+    }
+
+    private IEnumerator ActivarEnOrden()
+    {
+        foreach (GameObject obj in objetos)
+        {
+            if (obj != null)
+                obj.SetActive(true);
+
+            yield return new WaitForSeconds(tiempoDemora);
+        }
+
+        if (dt != null)
+            dt.Activar = true;
     }
 
     private void SetObjetos(bool estado)
